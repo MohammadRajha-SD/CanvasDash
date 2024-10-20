e8f05c864cf9e7e8e3e788b601621295d3fda95b
MohammadRajha-SD <mohammadrajha2@gmail.com>
2024-10-20 06:45:54 -0700

    edit-news-widget

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -42,15 +42,7 @@
 @foreach(['map', 'covid19', 'weather', 'news', 'stockmarket'] as $widget)
 <div id="{{$widget}}-modal" class="hidden fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
     <div class="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-        <h2 class="text-xl font-bold mb-4">
-            Edit {{ucfirst($widget)}} Widget
-        </h2>
-        <p class="text-gray-700 mb-4">
-            Configure your {{ucfirst($widget)}} widget settings here.
-        </p>
-        <button class="close-modal bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
-            Close
-        </button>
+        @livewire('edit-news-widget', ['name' => $widget], key('edit_'. $widget))
     </div>
 </div>
 @endforeach
